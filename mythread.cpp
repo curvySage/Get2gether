5a3c0977f94c4ac40b99686e48165bb19c275f42
gregseda <31677674+gregseda@users.noreply.github.com>
2017-11-28 13:13:35 -0800

    Update mythread.cpp

--- mythread.cpp
+++ mythread.cpp
@@ -36,7 +36,7 @@ void MyThread::performExit() {
 // PURPOSE: overrides the run function.
 void MyThread::run() {
     while (!m_exit) {
-        sleep(2);       // in seconds. msleep() for msecs.
+        sleep(3);       // in seconds. msleep() for msecs.
         emit mysignal();
     }
 }
